025ff9efe11b277a0c608769e3c353546a125eca
Yasser Fadl <yasser.fadl@gmail.com>
2024-11-13 15:02:17 +0100

    chore: upgrading vramework

--- backend/config.ts
+++ backend/config.ts
@@ -1,6 +1,7 @@
 import { LogLevel } from '@vramework/core/services/logger'
 import { Config } from './application-types'
+import { CreateConfig } from '@vramework/core'
 
-export const config: Config = {
+export const getConfig: CreateConfig<Config> = async () => ({
   logLevel: LogLevel.debug,
-}
+})
